ea98533a3554fa58c29bda8dbe0048fb04ac60a6
LuisPineda <luis.pineda.revelo@udla.edu.ec>
2026-06-03 19:45:33 -0500

    Initial commit

---
+++ src/main/java/edu/udla/integration/progreso2/Progreso2Application.java
@@ -0,0 +1,13 @@
+package edu.udla.integration.progreso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progreso2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progreso2Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/udla/integration/progreso2/Progreso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udla.integration.progreso2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progreso2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
